c2377459ee4f81ad2b0c5530b1c52c927c1616f5
Pawan Upadhyay <141557022+pawansatoshi@users.noreply.github.com>
2026-08-14 18:01:07 +0530

    fix: mount multilingual provider around app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,19 +7,22 @@ import { ProfilePage } from "./pages/ProfilePage";
 import { EditProfilePage } from "./pages/EditProfilePage";
 import { DeveloperResourcesPage } from "./pages/DeveloperResourcesPage";
 import { TechnicalOverviewPage } from "./pages/TechnicalOverviewPage";
+import { LanguageProvider } from "./lib/i18n";
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/mint" element={<MintPage />} />
-        <Route path="/profile/:address" element={<ProfilePage />} />
-        <Route path="/edit" element={<EditProfilePage />} />
-        <Route path="/resources" element={<DeveloperResourcesPage />} />
-        <Route path="/technical" element={<TechnicalOverviewPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <LanguageProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/mint" element={<MintPage />} />
+          <Route path="/profile/:address" element={<ProfilePage />} />
+          <Route path="/edit" element={<EditProfilePage />} />
+          <Route path="/resources" element={<DeveloperResourcesPage />} />
+          <Route path="/technical" element={<TechnicalOverviewPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </LanguageProvider>
   );
 }
